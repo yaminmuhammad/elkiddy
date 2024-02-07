0eab564d5ca285d25681f480767fa822b7f4544b
yaminmuhammad <1910631170108@student.unsika.ac.id>
2024-02-07 07:53:30 +0700

    fix

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://elkiddy-production.up.railway.app/'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL'),
 
