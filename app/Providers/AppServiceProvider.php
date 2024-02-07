485f8dbbb35ed1bf9d98988baeb8864dbde73a21
yaminmuhammad <1910631170108@student.unsika.ac.id>
2024-02-07 08:17:35 +0700

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,9 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        if (env('APP_ENV') !== 'local') { {
-                URL::forceScheme('https');
-            }
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
         }
     }
 }
