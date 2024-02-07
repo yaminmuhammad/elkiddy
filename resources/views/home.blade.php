6de87cd4481c1d519878924ad7942b69d3f7f4d0
yaminmuhammad <1910631170108@student.unsika.ac.id>
2024-02-07 09:28:07 +0700

    change meta title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Responsive Kindergarten Website</title>
+  <title>Elkiddy - Early Childhood Education</title>
   <!-- font awesome cdn link  -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
   <!-- lightgallery css cdn link -->
@@ -340,7 +340,7 @@
     <div class="box-container">
 
       <div class="box">
-        <h3> <i class="fas fa-school"></i> Kindergarten </h3>
+        <h3> <i class="fas fa-school"></i> Elkiddy </h3>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo, unde</p>
       </div>
 
